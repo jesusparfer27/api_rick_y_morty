4d29cf9b8fc72c65576bc43b458837b0c7f4829d
Carlos <cvaquer99@alumnos.cei.es>
2024-07-23 11:31:02 +0200

    commit class

--- rick_and_morty_api/src/categories/Categories.jsx
+++ rick_and_morty_api/src/categories/Categories.jsx
@@ -1,19 +1,60 @@
-import { Episodes } from '../episodios/Episodes'
-import { Locations } from '../ubicaciones/Locations'
-import { Characters } from '../personajes/Characters'
-import { useState, useEffect } from 'react'
+// import { Episodes } from '../episodios/Episodes'
+// import { Locations } from '../ubicaciones/Locations'
+// import { Characters } from '../personajes/Characters'
+// import { useState, useEffect } from 'react'
+// import {BrowserRouter as Router, Route, Routes, Link, NavLink} from 'react-router-dom'
 import './categories.css'
 
 export const Categories = () => {
 
-    const [seccion, setSeccion] = useState("characters")
-    
+    // const [seccion, setSeccion] = useState("")
 
-return (
-        <main>
-            {seccion == "characters" && <Characters />}
-            {seccion == "locations" && <Locations />}
-            {seccion == "episodes" && <Episodes />}
-        </main>
-)
+
+    return (<></>
+
+        // <Router>
+        //     <main className='Container'>
+        //         <header className='Header'>
+        
+        //                 <h1 className='Header-Title'>Rick and Morty</h1>
+        //             <nav>
+        //                 <ul>
+        //                     {/* v1  */}
+        //                     {/* <li><button onClick={ () => (setSeccion("characters"))}>Personajes</button></li>
+        //                     <li><button onClick={ () => (setSeccion("characters"))}>Personajes</button></li>
+        //                     <li><button onClick={ () => (setSeccion("characters"))}>Personajes</button></li> */}
+
+        //                     {/* v2. Sistema nuevo con React Router */}
+        //                     {/* <li><Link to="/characters">Personajes</Link></li>
+        //                     <li><Link to="/episodes">Episodios</Link></li>
+        //                     <li><Link to="/locations">Ubicaciones</Link></li> */}
+
+        //                     {/* v3.  Navegación con ReactcRouter */}
+        //                     <li><NavLink to="/characters" className="Button-Link">Personajes</NavLink></li>
+        //                     <li><NavLink to="/episodes" className="Button-Link">Episodios</NavLink></li>
+        //                     <li><NavLink to="/locations" className="Button-Link">Ubicaciones</NavLink></li>
+
+        //                 </ul>
+        //             </nav>
+        //         </header>
+        //             {/* Aqui creamos un "switch" de useState seccion */}
+        //             {/* {seccion == "characters" && <Characters />}
+        //         {seccion == "locations" && <Locations />}
+        //         {seccion == "episodes" && <Episodes />} */}
+
+        //             {/* Esta versión utiliza react-router */}
+        //         <div className="Content">
+        //             {/* Posibles rutas */}
+        //             <Routes>
+        //                 <Route path="/personajes" element={<Characters />}/>
+        //                 <Route path="/episodios" element={<Episodes />}/>
+        //                 <Route path="/ubicaciones" element={<Locations />}/>
+        //                 {/* Ruta principal  */}
+        //                 <Route path="/" element={<Categories/>} />
+        //             </Routes>
+        //         </div>
+        //     </main>
+        // </Router>
+
+    )
 }
